e59454ef3790e742aac15086c070d20e89b89d6d
ROBOT_SHO <soarbear7@gmail.com>
2020-01-14 16:54:20 +0900

    Update UsersTableSeeder.php

--- iot-demo/database/seeds/UsersTableSeeder.php
+++ iot-demo/database/seeds/UsersTableSeeder.php
@@ -11,20 +11,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        /*
-        DB::table('users')->delete();
 
-        User::create([
-            'name' => 'yanagi',
-            'email' => 'yanagi@soarcloud.com',
-            'password' => bcrypt('password')
-        ]);*/
         DB::table('users')->truncate();
   
         DB::table('users')->insert([
             [
             'name' => 'yanagi',
-            'email' => 'yanagi@soarcloud.com',
+            'email' => 'xxx@gmail.com',
             'password' => Hash::make('yanagi'),
             ],
         ]);
